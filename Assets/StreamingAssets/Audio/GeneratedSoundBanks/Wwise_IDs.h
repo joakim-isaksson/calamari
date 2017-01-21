9d13f3e027f61c041d23cfc4983c14c3231a55ce
Mikko Kolehmainen <mikko.kolehmainen@gmail.com>
2017-01-21 19:24:16 +0200

    Dialogue/Music/Car sounds added to Wwise

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CAR_PASSBY = 4142440576U;
+        static const AkUniqueID GRANDMA_NEGATIVE = 3032610195U;
+        static const AkUniqueID GRANDMA_POSITIVE = 3544995775U;
+        static const AkUniqueID GRANDMA_VERY_NEGATIVE = 1911024840U;
+        static const AkUniqueID MAN_NEGATIVE = 911506639U;
+        static const AkUniqueID MAN_POSITIVE = 666034363U;
+        static const AkUniqueID MAN_VERY_NEGATIVE = 889047188U;
+        static const AkUniqueID NARRATOR_GO = 2474639067U;
+        static const AkUniqueID NARRATOR_NEGATIVE = 2862836492U;
+        static const AkUniqueID NARRATOR_NEUTRAL = 645232014U;
+        static const AkUniqueID NARRATOR_POSITIVE = 3301552740U;
+        static const AkUniqueID NARRATOR_READY = 3640189846U;
+        static const AkUniqueID PLAY_INGAME_MUSIC = 347845317U;
+        static const AkUniqueID PLAY_MENUANDCREDITS_MUSIC = 3540843768U;
+        static const AkUniqueID PLAY_STARTLEVEL_MUSIC = 2890725208U;
+        static const AkUniqueID WOMAN_NEGATIVE = 1024076021U;
+        static const AkUniqueID WOMAN_POSITIVE = 2318958257U;
+        static const AkUniqueID WOMAN_VERY_NEGATIVE = 3367768002U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN_SOUNDBANK = 2228651116U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
