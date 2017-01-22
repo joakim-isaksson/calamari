af861c4b30ecfb4d0c4ef7fa5334b5f4c44ed151
maxiaoxiao1991 <maxiaoxiao1991@gmail.com>
2017-01-22 12:59:51 +0200

    basic splash screen

--- Assets/Scripts/MenuUIController.cs
+++ Assets/Scripts/MenuUIController.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuUIController : MonoBehaviour {
 	public string GameSceneName;
 	public string CreditSceneName;
+	public GameObject SplashScreen;
+	public float SplashFadeSecond = 1;
+	public float SplashShowSecond = 1;
+	private static bool  started = false;
+
 	// Use this for initialization
 	void Start () {
 		SceneManager.sceneLoaded += delegate {
@@ -13,6 +19,11 @@ public class MenuUIController : MonoBehaviour {
 			});
 		};
 
+		if (started == false) {
+			started = true;
+			SplashScreen.SetActive (true);
+			StartCoroutine (PlaySplashScreen());
+		}
 
 	}
 	
@@ -44,5 +55,39 @@ public class MenuUIController : MonoBehaviour {
 	public void PointerExitSound(){
 		AkSoundEngine.PostEvent ("Menu_Button_Release", gameObject);
 		Debug.Log ("button Exit Sound");
+	}
+
+
+	private IEnumerator PlaySplashScreen(){
+		
+		Image image = SplashScreen.GetComponent <Image> ();
+		image.color = Color.black;
+		float progress = 0;
+		while (image.color.r < 0.99f)
+		{
+			progress += (Time.deltaTime / SplashFadeSecond);
+			Debug.Log (progress);
+			float t = Mathf.Lerp (0.0f, 1.0f, progress);
+			image.color = new Color(t, t, t, 1);
+			yield return null;
+
+		}
+
+		yield return new WaitForSeconds (SplashShowSecond);
+
+		progress = 0;
+		while (image.color.r >0.01f)
+		{
+			progress += (Time.deltaTime / SplashFadeSecond);
+			Debug.Log (progress);
+			float t = Mathf.Lerp (1.0f, 0.0f, progress);
+			image.color = new Color(t, t, t, 1);
+			yield return null;
+
+		}
+
+		image.color = new Color(1, 1, 1, 1.0f);
+		SplashScreen.SetActive(false);
+
 	}
 }
